e1ef1c4e41dd60e70cae57b7b74ff6cdf8c00d1d
Gabriele Bonetti <gabriele.bonetti@gmail.com>
2017-08-09 01:38:18 +0200

    Add docopt project structure

---
+++ README.rst
@@ -0,0 +1,30 @@
+===============================
+shpotipy
+===============================
+
+.. image:: https://badge.fury.io/py/shpotipy.png
+    :target: http://badge.fury.io/py/shpotipy
+
+.. image:: https://travis-ci.org/g-bon/shpotipy.png?branch=master
+        :target: https://travis-ci.org/g-bon/shpotipy
+
+.. image:: https://pypip.in/d/shpotipy/badge.png
+        :target: https://crate.io/packages/shpotipy?version=latest
+
+
+A python based command line inteface for Spotify.
+
+Features
+--------
+
+* TODO
+
+Requirements
+------------
+
+- Python >= 2.6 or >= 3.3
+
+License
+-------
+
+MIT licensed. See the bundled `LICENSE <https://github.com/g-bon/shpotipy/blob/master/LICENSE>`_ file for more details.
--- shpotipy.py
+++ shpotipy.py
@@ -1,152 +1,191 @@
-import json
-import requests
-from requests.auth import HTTPBasicAuth
-from configurations import client_id, client_secret
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Shpotipy.
+
+
+Usage:
+    shpotipy play [(album | artist | list | uri) <query>]
+    shpotipy next
+    shpotipy prev
+    shpotipy replay
+    shpotipy pos [<time>]
+    shpotipy pause
+    shpotipy quit
+    shpotipy vol [show | up | down | set <amount>]
+    shpotipy status
+    shpotipy share
+    shpotipy share url
+    shpotipy share uri
+    shpotipy toggle shuffle
+    shpotipy toggle repeat
+'''
+
+from __future__ import unicode_literals, print_function, absolute_import
+from docopt import docopt, DocoptExit
 from subprocess import Popen, PIPE
+from termcolor import colored
+from configurations import client_id, client_secret
 
-body = {'grant_type': 'client_credentials'}
-authUrl = "https://accounts.spotify.com/api/token"
-
-
-def singleton(cls, *args, **kw):
-    instances = {}
-
-    def _singleton():
-        if cls not in instances:
-            instances[cls] = cls(*args, **kw)
-        return instances[cls]
-    return _singleton
+__version__ = "0.1.0"
+__author__ = "Gabriele Bonetti"
+__license__ = "MIT"
 
 
-@singleton
-class Shpotipy:
-    def __init__(self):
-        self.authToken = self.authenticate(client_id, client_secret)
-
-    def authenticate(self, usr, pwd):
-        response = requests.post(authUrl, auth=HTTPBasicAuth(usr, pwd), data=body)
-        return response.json().get("access_token", None)
-
-
-    def set_client_id(self, client_id):
-        pass
-
-
-    def set_client_secret(self, client_secret):
-        pass
-
-    def search_and_play(self, type='track', query=None):
-        assert type is None or type in ['track', 'album', 'artist', 'list', 'uri']
-        searchUrl = "https://api.spotify.com/v1/search?q={}&type={}".format(query, type)
-        headers = {'Authorization': "Bearer {}".format(self.authToken)}
-        response = requests.get(searchUrl, headers=headers)
-        items = json.loads(response.content)["tracks"]["items"]
-        if len(items) is not 0:
-            songURI = items[0]['uri']
-            self.run_osa_script(Commands.playtrack.format(songURI))
-            return items[0]
-        return None
-
-    def get_state(self):
-        returncode, state, stderr = self.run_osa_script(Commands.getstate)
-        print("Spotify is currently {}".format(state))
-
-
-    def get_artist(self):
-        returncode, artist, stderr = self.run_osa_script(Commands.getartist)
-        print("Artist: {}".format(artist))
-
-
-    def get_album(self):
-        returncode, album, stderr = self.run_osa_script(Commands.getalbum)
-        print("Album: {}".format(album))
-
-
-    def get_track(self):
-        returncode, track, stderr = self.run_osa_script(Commands.gettrack)
-        print("Track: {}".format(track))
-
-
-    def get_duration(self):
-        returncode, duration, stderr = self.run_osa_script(Commands.getduration)
-        return duration
-
-
-    def get_position(self):
-        returncode, position, stderr = self.run_osa_script(Commands.getposition)
-        return position
-
-    def play_pause(self):
-        print("PlayPausing")
-        self.run_osa_script(Commands.playpause)
-
-
-    def show_complete_status(self):
-        self.get_state()
-        self.get_artist()
-        self.get_album()
-        self.get_track()
-        print("Position: {} / {}".format(self.get_position(), self.get_duration()))
-
-
-    def run_osa_script(self, script):
-        p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-        stdout, stderr = p.communicate(script)
-        return p.returncode, stdout.strip(), stderr
-
-
-    def showHelp(self):
-        pass
-
-
-    def getStatus(self):
-        pass
-
-
-class Commands:
+class Osa:
     getstate = 'tell application "Spotify" to player state as string'
     getartist = 'tell application "Spotify" to artist of current track as string'
     getalbum = 'tell application "Spotify" to album of current track as string'
     gettrack = 'tell application "Spotify" to name of current track as string'
     playtrack = 'tell application "Spotify" to play track "{}"'
+    playprevioustrack = '''tell application "Spotify"
+                               set player position to 0
+                               previous track
+                           end tell'''
+    playnexttrack = 'tell application "Spotify" to next track'
+    playfromstart = 'tell application "Spotify" to set player position to 0'
+    playpause = 'tell application "Spotify" to playpause'
+    quit = 'tell application "Spotify" to quit'
     activate = 'tell application "Spotify" to activate'
     play = 'tell application "Spotify" to play'
     state = 'tell application "Spotify" to player state as string'
-    playpause = 'tell application "Spotify" to playpause'
-    quit = 'tell application "Spotify" to quit'
-    nexttrack = 'tell application "Spotify" to next track'
-    playfromstart = 'tell application "Spotify" to set player position to 0'
     setvolume = 'tell application "Spotify" to set sound volume to {}'
     noshuffle = 'tell application "Spotify" to set shuffling to not shuffling'
     shuffle = 'tell application "Spotify" to shuffling'
     norepeat = 'tell application "Spotify" to set repeating to not repeating'
     repeat = 'tell application "Spotify" to repeating'
+    getvolume = 'tell application "Spotify" to sound volume as integer'
     getduration = '''
             tell application "Spotify"
-            set durSec to (duration of current track / 1000) as text
-            set tM to (round (durSec / 60) rounding down) as text
-            if length of ((durSec mod 60 div 1) as text) is greater than 1 then
-                set tS to (durSec mod 60 div 1) as text
-            else
-                set tS to ("0" & (durSec mod 60 div 1)) as text
-            end if
-            set myTime to tM as text & ":" & tS as text
+                set durSec to (duration of current track / 1000) as text
+                set tM to (round (durSec / 60) rounding down) as text
+                if length of ((durSec mod 60 div 1) as text) is greater than 1 then
+                    set tS to (durSec mod 60 div 1) as text
+                else
+                    set tS to ("0" & (durSec mod 60 div 1)) as text
+                end if
+                set myTime to tM as text & ":" & tS as text
             end tell
     '''
     getposition = '''
             tell application "Spotify"
-            set pos to player position
-            set nM to (round (pos / 60) rounding down) as text
-            if length of ((round (pos mod 60) rounding down) as text) is greater than 1 then
-                set nS to (round (pos mod 60) rounding down) as text
-            else
-                set nS to ("0" & (round (pos mod 60) rounding down)) as text
-            end if
-            set nowAt to nM as text & ":" & nS as text
+                set pos to player position
+                set nM to (round (pos / 60) rounding down) as text
+                if length of ((round (pos mod 60) rounding down) as text) is greater than 1 then
+                    set nS to (round (pos mod 60) rounding down) as text
+                else
+                    set nS to ("0" & (round (pos mod 60) rounding down)) as text
+                end if
+                set nowAt to nM as text & ":" & nS as text
             end tell
     '''
 
 
-if __name__ == "__main__":
-    shpotipy = Shpotipy()
-    song = shpotipy.search_and_play('track', "stu larsen chicago")
+def run_osa_script(script):
+    p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    stdout, stderr = p.communicate(script)
+    return stdout.strip()
+
+
+def status(args):
+    print("Spotify is currently {}".format(run_osa_script(Osa.getstate)))
+    print("Artist: {}".format(run_osa_script(Osa.getartist)))
+    print("Album: {}".format(run_osa_script(Osa.getalbum)))
+    print("Track: {}".format(run_osa_script(Osa.gettrack)))
+    pos()
+
+def next_track(args):
+    run_osa_script(Osa.playnexttrack)
+
+
+def previous_track(args):
+    run_osa_script(Osa.playprevioustrack)
+
+
+def play(args):
+    pass
+
+
+def replay(args):
+    run_osa_script(Osa.playfromstart)
+
+
+def pos(args):
+    print("Position: {} / {}".format(run_osa_script(Osa.getposition), run_osa_script(Osa.getduration)))
+
+
+def pause(args):
+    run_osa_script(Osa.playpause)
+
+
+def quit_spotify(args):
+    run_osa_script(Osa.quit)
+
+
+def vol(args):
+    vol_step = 10
+
+    if args['show']:
+        print(run_osa_script(Osa.getvolume))
+
+    elif args['up']:
+        vol = run_osa_script(Osa.getvolume)
+        run_osa_script(Osa.setvolume.format(vol + vol_step))
+        print("Volume: {}".format(vol+vol_step))
+
+    elif args['down']:
+        vol = run_osa_script(Osa.getvolume)
+        run_osa_script(Osa.setvolume.format(vol - vol_step))
+        print("Volume: {}".format(vol-vol_step))
+
+    elif args['set']:
+        try:
+            vol = int(args['<amount>'])
+        except ValueError:
+            print(colored("Volume value must be a number between 0 and 100", "red"))
+            raise DocoptExit
+        if 0 <= vol <= 100:
+            run_osa_script(Osa.setvolume.format(args['<amount>']))
+            print("Volume: {}".format(vol))
+        else:
+            print(colored("Volume value must be a number between 0 and 100", "red"))
+            raise DocoptExit
+
+def share(args):
+    pass
+
+
+def toggle_shuffle(args):
+    pass
+
+
+def toggle_repeat(args):
+    pass
+
+
+def main():
+    '''Main entry point for the shpotipy CLI.'''
+    args = docopt(__doc__)
+    # print(args)
+
+    command = next(arg for arg in args if arg in ['play', 'status', 'next', 'prev', 'replay', 'pos', 'pause', 'quit', 'vol', 'share', 'shuffle', 'repeat'] and args[arg] is True)
+
+    {
+        'play': play,
+        'status': status,
+        'next': next_track,
+        'prev': previous_track,
+        'replay': replay,
+        'pos': pos,
+        'pause': pause,
+        'quit': quit_spotify,
+        'vol': vol,
+        'share': share,
+        'shuffle': toggle_shuffle,
+        'repeat': toggle_repeat
+    }[command](args)
+
+
+if __name__ == '__main__':
+    main()
