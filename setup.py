ba0e8014ebff14f961d20a2f3c90ef70b6d03d6c
Gabriele Bonetti <gabriele.bonetti@gmail.com>
2017-08-19 18:02:26 +0200

    simplified setup.py and remove old spotipy

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ import re
 import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
-
-
-REQUIRES = [
-    'docopt', 'colored', 'pyperclip', 'requests', 'future'
-]
+from shpotipy.shpotipy import __version__
 
 
 class PyTest(TestCommand):
@@ -21,34 +17,13 @@ class PyTest(TestCommand):
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
-
-def find_version(fname):
-    '''Attempts to find the version number in the file names fname.
-    Raises RuntimeError if not found.
-    '''
-    version = ''
-    with open(fname, 'r') as fp:
-        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
-        for line in fp:
-            m = reg.match(line)
-            if m:
-                version = m.group(1)
-                break
-    if not version:
-        raise RuntimeError('Cannot find version information')
-    return version
-
-__version__ = find_version("shpotipy/shpotipy.py")
-
-
-def read(fname):
-    with open(fname) as fp:
-        content = fp.read()
-    return content
+REQUIRES = [
+    'docopt', 'colored', 'pyperclip', 'requests', 'future'
+]
 
 setup(
     name='shpotipy',
-    version="0.4",
+    version=__version__,
     description='A python based command line interface for Spotify.',
     author='Gabriele Bonetti',
     author_email='gabriele.bonetti@gmail.com',
